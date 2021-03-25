d779e43cf9dc68e728d1fffddab95b5484cc53d5
novvan <ianallengeier@gmail.com>
2021-03-25 01:04:41 -0300

    progress

---
+++ src/components/Twoot.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="twoot" @click="likeTwoot(twoot.id)">
+    <div class="twoot__user">
+      {{ username }}
+    </div>
+    <div class="twoot__content">
+      {{ twoot.data }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TwootItem',
+  props: {
+    twoot: {type: Object, required: true},
+    username: {type: String, required: true}
+  },
+  methods: {
+    likeTwoot(id) {
+      this.$emit('liked', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.twoot {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  border: 1px solid whitesmoke;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
+}
+.twoot:hover {
+  transform: scale(1.1, 1.1);
+}
+
+.twoot__user {
+  font-weight: bold;
+}
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,20 +8,57 @@
       <div class="userProfile__followers">
         <strong>Followers: </strong> {{ followers }}
       </div>
+      <form
+        action=""
+        class="userProfile__newTwootForm"
+        @submit.prevent="createNewTwoot"
+      >
+        <label for="newTwoot" class="userProfile__newTwootLabel">
+          <strong>New Twoot</strong>
+        </label>
+        <textarea
+          id="newTwoot"
+          rows="5"
+          placeholder="Type your new twoot!"
+          v-model="newTwootContent"
+        />
+        <div class="userProfile__newTwootType">
+          <label for="newTwootType" class="userProfile__newTwootTypeLabel">
+            Type:
+          </label>
+          <select
+            id="newTwootType"
+            class="userProfile__newTwootType"
+            v-model="newTwootType"
+          >
+            <option
+              :value="option.value"
+              v-for="(option, index) in twootOptions"
+              :key="index"
+            >
+              {{ option.value }}
+            </option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Twoot!
+        </button>
+      </form>
     </div>
     <div v-if="user.twoots.length > 0" class="userProfile__twootsWrapper">
       <TwootItem
-        v-for="twoot in user.twoots"
+        v-for="twoot in latestTwoots"
         :key="twoot.id"
         :twoot="twoot"
-        :username="user.username"
+        :username="computedUserName"
+        @liked="toggleLike"
       />
     </div>
   </div>
 </template>
 
 <script>
-import TwootItem from './TwootItem'
+import TwootItem from './Twoot'
 
 export default {
   name: 'UserProfile',
@@ -30,7 +67,17 @@ export default {
   },
   data() {
     return {
+      newTwootContent: null,
+      newTwootType: 'Instant',
       followers: 0,
+      twootOptions: [
+        {
+          value: 'Draft'
+        },
+        {
+          value: 'Instant'
+        }
+      ],
       user: {
         id: 1,
         username: 'NoVvaN',
@@ -47,17 +94,17 @@ export default {
           },
           {
             id: 2,
-            data: 'WHAT A FANTASTIC DAY',
+            data: 'New vue app running',
             likes: 0
           },
           {
             id: 3,
-            data: 'WHAT A FANTASTIC DAY',
+            data: 'Hello World',
             likes: 0
           },
           {
             id: 4,
-            data: 'WHAT A FANTASTIC DAY',
+            data: 'Pedro pascal sucks',
             likes: 0
           }
         ]
@@ -74,11 +121,29 @@ export default {
   computed: {
     fullName() {
       return `${this.user.firstName} ${this.user.lastName}`
+    },
+    computedUserName() {
+      return `@${this.user.username}`
+    },
+    latestTwoots() {
+      return this.user.twoots.slice(0).reverse()
     }
   },
   methods: {
     followUser() {
       this.followers++
+    },
+    toggleLike(id) {
+      console.log('Favourited tweert numbr: ' + id)
+    },
+    createNewTwoot() {
+      if (this.newTwootContent != null && this.newTwootType !== 'Draft')
+        this.user.twoots.push({
+          id: this.user.twoots.length + 1,
+          data: this.newTwootContent,
+          likes: 0
+        })
+      this.newTwootContent = ''
     }
   },
   mounted() {
@@ -94,13 +159,12 @@ h1 {
 .userProfile {
   display: grid;
   grid-template-columns: 1fr 3fr;
-  width: 100%;
-  padding: 50 5%;
+  grid-gap: 50px;
+  padding: 50px 5%;
 }
 .userProfile__userPanel {
   display: flex;
   flex-direction: column;
-  margin-right: 50px;
   padding: 20px;
   background-color: white;
   border-radius: 5px;
@@ -108,11 +172,21 @@ h1 {
 }
 .userProfile__adminBadge {
   color: white;
+  font-size: 12px;
   font-weight: bold;
   margin-right: auto;
   padding: 2.5px 5px;
-  background-color: #e74c3c;
+  background-color: #9b59b6;
   border-radius: 5px;
-  border: 1px solid #c0392b;
+  border: 1px solid #8e44ad;
+}
+.userProfile__twootsWrapper {
+  display: grid;
+  grid-gap: 10px;
+}
+.userProfile__newTwootForm {
+  padding-top: 25px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
